a536c1e81a37bcfaed2fdd40845ca0905488baad
Evey Dee <eveydee@users.noreply.github.com>
2017-07-20 17:01:11 +0800

    More magic!

--- src/mu.jl
+++ src/mu.jl
@@ -7,7 +7,7 @@ macro μ(words::Symbol...)
     for word in words
         token = μagic(word, token)
     end
-    return esc(:((μ) -> $token))
+    return esc(token)
 end
 
 function μagic(word::Symbol, token)
@@ -20,25 +20,36 @@ function μagic(word::Symbol, token)
 end
 
 Table = Dict([
-    :d => :/
-    :p => :*
+    :a => :+
+    :e => :^
+    :i => :*
+    :o => :/
     :q => :\
-    :s => :+
-    :x => :^
-    :z => :-
+    :u => :-
 ])
 
 a(glyphs, token) = token
-b(glyphs, token) = :(x = $token; (x..., x...))
-d(glyphs, token) = (f = Table[pop!(glyphs)]; :(reduce($f, $token)))
+b(glyphs, token) = :(x = $token; (x, x))
+d(glyphs, token) = (length(glyphs) > 0 ? (f = Table[pop!(glyphs)]; :(reduce($f, $token))) :
+                    Expr(token.head, :reduce, token.args[2:end]...))
 e(glyphs, token) = (:(x = $token; ($(eval(Expr(:call, pop!(glyphs), glyphs, :(:x)))), $(eval(Expr(:call, pop!(glyphs), glyphs, :(:x)))))))
+f(glyphs, token) = (fname = Symbol(glyphs...); empty!(glyphs); :($fname(μ) = $token))
+g(glyphs, token) = (fname = Symbol(glyphs...); empty!(glyphs); :($fname(μ...) = $token))
+h(glyphs, token) = (:(first($token)))
 
+k(glyphs, token) = :(collect($token))
+l(glyphs, token) = :(last($token))
+
+m(glyphs, token) = (f = Table[pop!(glyphs)]; :(map($f, $token)))
 n(glyphs, token) = :(x = $token; (one(x), x))
 p(glyphs, token) = :(prod($token))
-q(glyphs, token) = :(prod($token))
+q(glyphs, token) = Expr(token.head, reverse(token.args))
 r(glyphs, token) = (reverse!(glyphs); token)
 s(glyphs, token) = :(sum($token))
+t(glyphs, token) = (:($token[2:end]))
 
 u(glyphs, token) = :(x = $token; UnitRange(x...))
+w(glyphs, token) = (n = Int(first("$(pop!(glyphs))")); :($token, $n))
+!(glyphs::Vector{Symbol}, token) = (:())
 
 end
